65c47e344ac6bd0e39c46e5be46c081ee1461796
Nguyễn Ngọc Bình <47907431+nguyenngocbinh@users.noreply.github.com>
2020-01-03 17:58:39 +0700

    update

--- R/psi.r
+++ R/psi.r
@@ -10,35 +10,42 @@ library(tidyverse)
 
 psi <- function(actual, expected){
   
-  list(actual = actual, expected = expected) %>% 
+  tbl <- list(actual = actual, expected = expected) %>% 
     # map(factor, levels = lbl, labels = lbl) %>% 
     map(table) %>% 
     map(prop.table) %>% 
-    map(as.data.frame) %>% 
+    map(as.data.frame, stringsAsFactors=FALSE) %>% 
     reduce(full_join, by = "Var1") %>% 
-    mutate(psi = (Freq.x-Freq.y)*(log(Freq.x)-log(Freq.y))) %>% 
-    pull(psi) %>% 
-    sum() -> psi
-
-  return(psi)
+    mutate(sub_psi = (Freq.x-Freq.y)*(log(Freq.x)-log(Freq.y))) %>% 
+    mutate(psi = sum(sub_psi)) %>% 
+    select(Range = Var1, 
+           Trainning = Freq.y, 
+           Actual = Freq.x, 
+           psi) %>% 
+    mutate(Trainning = paste0(round(Trainning * 100, 1), "%"),
+           Actual = paste0(round(Actual * 100, 1), "%"),
+           psi = round(psi, 3),
+           tag = case_when(psi < 0.1 ~ "green", 
+                           psi < 0.25 ~ "yellow", 
+                           TRUE ~ "red"))
+  
+  
+  return(tbl)
 }
-
 
 # Hàm tính PSI cho cả bộ dữ liệu
 psi_df <- function(dat_train, dat_test, var_skip = NULL){
   same_name <- names(dat_test) %>%
     intersect(names(dat_train)) %>%
     setdiff(var_skip)
-
+  
   sl <- function(x){
-    psi(dat_train[[x]], dat_test[[x]]) %>% as.data.frame() %>% set_names("psi") %>% return()
+    psi(expected = dat_train[[x]], actual = dat_test[[x]]) %>% return()
   }
   
   same_name %>% 
     set_names(same_name) %>%  
-    map_dfr(sl, .id = "variables") %>%
-    mutate(tag = case_when(psi < 0.1 ~ "green", psi < 0.25 ~ "yellow", TRUE ~ "red")) %>% 
+    map_dfr(sl, .id = "Variables") %>%  
     return()
-    
+  
 }
-
